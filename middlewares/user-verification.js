bfe2de2417c45c934c1d70a44a7dab3450acae2f
tanu-sh1201 <tanu@gkmit.co>
2022-11-22 18:59:16 +0530

    user-create-API-update

--- middlewares/user-verification.js
+++ middlewares/user-verification.js
@@ -1,21 +1,20 @@
-const models = require('../models');
+const models = require("../models");
 
 module.exports = {
-    verifyUser: async (req, res, next) => {
-        try {
-            const user = await models.User.findOne({
-                where: {
-                    email: req.email
-                }
-            })
-            if (user.is_admin) {
-                next();
-            } else {
-                return res.status(403).json({ response: 'Access denied' });
-            }
-        } catch (err) {
-            return res.status(500).json({ error: `Something went wrong!` });
-        }
-
+  verifyUser: async (req, res, next) => {
+    try {
+      const user = await models.User.findOne({
+        where: {
+          email: req.email,
+        },
+      });
+      if (user == "CEO") {
+        next();
+      } else {
+        return res.status(403).json({ response: "Access denied" });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: `Something went wrong!` });
     }
-}
+  },
+};
