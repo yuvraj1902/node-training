0bb3b294a2fae5e497fb22b4edcdf26e8791be20
divyanshigkmit <divyanshi@gkmit.co>
2022-11-24 15:29:55 +0530

    Refactor role model and migration

--- models/user.js
+++ models/user.js
@@ -61,6 +61,56 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: true,
       },
     },
+<<<<<<< HEAD
+=======
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isNumeric: true,
+      unique: true
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlphanumeric: true
+    },
+    token: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    token_expiration: {
+      type: DataTypes.BIGINT,
+      allowNull: true
+    },
+    role: {
+      type: DataTypes.ENUM(['CEO', 'LEAD', 'EMPLOYEE', 'INTERN']),
+      allowNull: false,
+    },
+    is_delete: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: '0'
+    },
+  },
+>>>>>>> 9b7b066 (Refactor role model and migration)
 
     {
       sequelize,
