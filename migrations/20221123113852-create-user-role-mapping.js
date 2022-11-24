a25e2fdd1d23bf9e722d36bdc02ac866a7ea2935
divyanshigkmit <divyanshi@gkmit.co>
2022-11-24 15:13:58 +0530

    Resolve conflicts by accepting incoming changes

--- migrations/20221123113852-create-user-role-mapping.js
+++ migrations/20221123113852-create-user-role-mapping.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, DataTypes) {
-    await queryInterface.createTable("user_role_mapping", {
+    await queryInterface.createTable('user_role_mapping', {
       id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.literal("uuid_generate_v4()"),
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, DataTypes) {
-    await queryInterface.dropTable("user_role_mapping");
-  },
+    await queryInterface.dropTable('user_role_mapping');
+  }
 };
