729c354c673552ce2027f7a4c3760b3ebdddd90a
tanu-sh1201 <tanu@gkmit.co>
2022-11-24 11:02:45 +0530

    script for user

--- admin-seeder.js
+++ admin-seeder.js
@@ -27,14 +27,42 @@ prompt.start();
          name: "organization",
          required:true
      },
+     {
+       name: "google_id",
+       required:true
+     },
+     {
+       name: "phone",
+       required:true
+     },
+     {
+       name: "user_name",
+       required:true
+     },
+     {
+       name: "source",
+       required:true
+     },
+     {
+       name: "role_title",
+       description: colors.magenta("Role should be admin or user"),
+       required: true,
+       
+     }, {
+       name: "designation_user_mapping",
+       required:true
+     }
    ],
-   function (err, result) {
-     //
-     // Log the results.
-     //
-     console.log("Command-line input received:");
-     console.log("  username: " + result.username);
-     console.log("  password: " + result.password);
+   async function (err, result) {
+    const user = await models.User.create({
+            first_name: result.first_name,
+            last_name: result.last_name,
+            email: result.email,
+            phone: result.phone,
+            user_name:result.user_name,
+            password: await hash(result.password, 10),
+    });
+     
    }
  );
    
