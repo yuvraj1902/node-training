9d7fc7fd33562c404d88c218cdccd96a59d454dc
tanu-sh1201 <tanu@gkmit.co>
2022-11-25 11:17:51 +0530

    assocition changes

---
+++ models/user_role_mapping.js
@@ -0,0 +1,23 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserRoleMapping extends Model {
+    static associate(models) {}
+  }
+  UserRoleMapping.init(
+    {
+      user_id: {
+        type: DataTypes.UUID,
+      },
+      role_id: {
+        type: DataTypes.UUID,
+      },
+    },
+    {
+      sequelize,
+      tableName: "user_role_mapping",
+      modelName: "UserRoleMapping",
+    }
+  );
+  return UserRoleMapping;
+};
