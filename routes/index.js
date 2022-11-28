10fea4b8bbadf01b50ffaabb13cd6b2730c5f7dc
divyanshigkmit <divyanshi@gkmit.co>
2022-11-28 15:15:33 +0530

    Create addReportee function

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./user.route");
-const roleRoutes = require("./role.route");
+// const roleRoutes = require("./role.route");
 const userReporteeRoutes = require("./userReportee.route");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/", userRoutes);
-app.use("/", roleRoutes);
+// app.use("/", roleRoutes);
 app.use("/", userReporteeRoutes);
 
 
