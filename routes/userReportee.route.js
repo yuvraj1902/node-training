abd02460658336e4c4f85abfcb283f0913353a43
divyanshigkmit <divyanshi@gkmit.co>
2022-11-28 15:15:33 +0530

    Create addReportee function

--- routes/userReportee.route.js
+++ routes/userReportee.route.js
@@ -8,5 +8,6 @@ const validator = require("../validators")
 const genericResponse = require("../helper/generic-response")
 
 
-router.post('/addReportee/:id', validator.addReporteeValidator.addReporteeSchema, controllers.UserReportee.addReportee, genericResponse.sendResponse);
+// router.post('/addReportee/:id', validator.addReporteeValidator.addReporteeSchema, controllers.UserReportee.addReportee, genericResponse.sendResponse);
+router.post('/adminAddReportee', validator.addReporteeValidator.adminAddReporteeSchema, controllers.UserReportee.adminAddReportee, genericResponse.sendResponse)
 module.exports = router;
--- validators/userReportee.validator.js
+++ validators/userReportee.validator.js
@@ -16,5 +16,23 @@ module.exports = {
         } catch (error) {
             return res.status(500).json({ message: 'Something went wrong!' });
         }
+    },
+
+    adminAddReporteeSchema: async (req, res, next) => {
+        try {
+            const checkSchema = Joi.object({
+                manager_id: Joi.string().guid(),
+                reportee_id: Joi.string().guid()
+            });
+            const result = checkSchema.validate(req.body);
+            if (result.error) {
+                return res.status(400).json(result.error.details[0].message);
+            } else {
+                next();
+            }
+
+        } catch (error) {
+            return res.status(500).json({ message: 'Something went wrong!' });
+        }
     }
 }
