f5e9e8c8b713b69ed7ddbed77ef1e71810f03793
divyanshigkmit <divyanshi@gkmit.co>
2022-11-28 15:15:33 +0530

    Create addReportee function

--- controllers/userReportee.controller.js
+++ controllers/userReportee.controller.js
@@ -1,10 +1,17 @@
 const models = require("../models");
-const { addReportee } = require('../services/userReportee.service');
+const { addReportee, adminAddReportee } = require('../services/userReportee.service');
 
 module.exports = {
 
     addReportee: async (req, res, next) => {
         addReportee(req.params.id, req.body.id, (statusCode, result) => {
+            req.statusCode = statusCode;
+            req.result = result;
+            next();
+        });
+    },
+    adminAddReportee: async (req, res, next) => {
+        adminAddReportee(req.body, (statusCode, result) => {
             req.statusCode = statusCode;
             req.result = result;
             next();
--- services/userReportee.service.js
+++ services/userReportee.service.js
@@ -1,34 +1,46 @@
 const models = require("../models");
 
-module.exports = {
-    addReportee: async (manager_id, reportee_id, callback) => {
-        try {
-            // check for valid request
-            const existingManager = await models.User.findOne({ where: { id: manager_id }, include: models.Designation });
-            if (!existingManager) return callback(404, { response: `User not found` });
 
-            const existingReportee = await models.User.findOne({ where: { id: reportee_id }, include: models.Designation });
-            if (!existingReportee) return callback(404, { response: `User not found` });
+const addReportee = async (manager_id, reportee_id, callback) => {
+    try {
+        // check for valid request
+        const existingManager = await models.User.findOne({ where: { id: manager_id }, include: models.Designation });
+        if (!existingManager) return callback(404, { response: `User not found` });
 
-            if (existingManager.Designations[0].designation_code < existingReportee.Designations[0].designation_code) {
-                const existingField = await models.UserReportee.findOne({
-                    where: {
-                        manager_id: manager_id,
-                        reportee_id: reportee_id
-                    }
-                })
-                if (existingField) return callback(409, { response: `Already existing relation` });
-                const reporteeAdded = await models.UserReportee.create({
+        const existingReportee = await models.User.findOne({ where: { id: reportee_id }, include: models.Designation });
+        if (!existingReportee) return callback(404, { response: `User not found` });
+
+        if (existingManager.Designations[0].designation_code < existingReportee.Designations[0].designation_code) {
+            const existingField = await models.UserReportee.findOne({
+                where: {
                     manager_id: manager_id,
-                    reportee_id: reportee_id,
-                })
-                return callback(200, { response: reporteeAdded });
-            } else {
-                return callback(422, { message: `Unprocessable` });
-            }
-
-        } catch (err) {
-            return callback(500, { error: `Something went wrong!` });
+                    reportee_id: reportee_id
+                }
+            })
+            if (existingField) return callback(409, { response: `Already existing relation` });
+            const reporteeAdded = await models.UserReportee.create({
+                manager_id: manager_id,
+                reportee_id: reportee_id,
+            })
+            return callback(200, { response: reporteeAdded });
+        } else {
+            return callback(422, { message: `Unprocessable` });
         }
+
+    } catch (err) {
+        return callback(500, { error: `Something went wrong!` });
+    }
+}
+
+module.exports = {
+    // addReportee: async (manager_id, reportee_id, callback) => {
+    //     
+    // },
+
+    adminAddReportee: async (body, callback) => {
+        const { manager_id, reportee_id } = body;
+
+        return addReportee(manager_id, reportee_id, callback);
+
     }
 }
