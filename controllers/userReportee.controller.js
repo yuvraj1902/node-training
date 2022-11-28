329bf30d31338a07470f2cf7e65f38c834a2f67a
divyanshigkmit <divyanshi@gkmit.co>
2022-11-28 15:15:33 +0530

    Create addReportee function

--- controllers/userReportee.controller.js
+++ controllers/userReportee.controller.js
@@ -1,23 +1,19 @@
-const models = require("../models");
-const {
-  addReportee,
-  adminAddReportee,
-} = require("../services/userReportee.service");
+const { addReportee, adminAddReportee } = require('../services/userReportee.service');
 
 module.exports = {
-  addReportee: async (req, res, next) => {
-    addReportee(req.params.id, req.body.id, (statusCode, result) => {
-      req.statusCode = statusCode;
-      req.result = result;
-      next();
-    });
-  },
-  adminAddReportee: async (req, res, next) => {
-    console.log(req.body);
-    adminAddReportee(req.manager_id,req.reportee_id,(statusCode, result) => {
-      req.statusCode = statusCode;
-      req.result = result;
-      next();
-    });
-  },
-};
+
+    addReportee: async (req, res, next) => {
+        addReportee(req.params.id, req.body.id, (statusCode, result) => {
+            req.statusCode = statusCode;
+            req.result = result;
+            next();
+        });
+    },
+    adminAddReportee: async (req, res, next) => {
+        adminAddReportee(req.body, (statusCode, result) => {
+            req.statusCode = statusCode;
+            req.result = result;
+            next();
+        });
+    }
+}
