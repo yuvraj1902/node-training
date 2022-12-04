e8cf3dc01df650a2ae5836be5f043ba14708599c
raghvendragkmit <raghvendra@gkmit.co>
2022-12-04 13:07:12 +0530

    refactor config.js

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 console.log(path.resolve(__dirname, '../.env'));
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "port":process.env.DB_PORT,
     "dialect": "postgres",
