ce67adc42d7fb130d40ba2d8cbc5123e6208d934
raghvendragkmit <raghvendra@gkmit.co>
2022-12-04 09:42:43 +0530

    refactor database  models, associations, migrations

--- migrations/20221203111012-create-user-role-mapping.js
+++ migrations/20221203111012-create-user-role-mapping.js
@@ -1,56 +1,49 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('user_role_mapping', {
+    await queryInterface.createTable("user_role_mapping", {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('uuid_generate_v4()')
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
       },
       user_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
           model: "user",
-          key:'id'
-        }
+          key: "id",
+        },
       },
       role_id: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
           model: "role",
-          key:'id'
-        }
+          key: "id",
+        },
       },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue:Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue:Sequelize.NOW
+        defaultValue: Sequelize.NOW,
       },
-<<<<<<< HEAD
-<<<<<<< HEAD
-      deleted_at:{
-=======
-      delete_at:{
->>>>>>> 3b79d83 (refactor database  models, associations, migrations)
-=======
-      delete_at:{
->>>>>>> 3b79d83 (refactor database  models, associations, migrations)
-         allowNull: true,
+
+      delete_at: {
+        allowNull: true,
         type: Sequelize.DATE,
-        defaultValue:null
-      }
+        defaultValue: null,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('user_role_mapping');
-  }
+    await queryInterface.dropTable("user_role_mapping");
+  },
 };
